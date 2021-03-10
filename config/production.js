0a0cca2f161805ec584370bfbab5e585b6a88b1d
Oluwakorede Moshood <kenykore@gmail.com>
2021-03-10 17:11:59 +0100

    Fixed issue with cors allowed domain

--- config/production.js
+++ config/production.js
@@ -4,7 +4,7 @@ const productionConfig = {
   port: process.env.PORT || 9700,
   node_environment: process.env.ENVIRONMENT,
   database_url: process.env.PRODUCTION_DB_URL,
-  frontend_url: process.env.DEV_FRONTEND_URL
+  frontend_url: process.env.PRODUCTION_FRONTEND_URL
 
 };
 
--- config/staging.js
+++ config/staging.js
@@ -4,7 +4,7 @@ const devConfig = {
   port: process.env.PORT || 9700,
   node_environment: process.env.ENVIRONMENT,
   database_url: process.env.STAGING_DB_URL,
-  frontend_url: process.env.DEV_FRONTEND_URL
+  frontend_url: process.env.STAGING_FRONTEND_URL
 
 };
 
